c5ec7b7b209a2f1033cd7f4d44f468373fdf46a7
chohancheol <45474814+chohancheol@users.noreply.github.com>
2023-06-17 15:02:31 +0900

    Update Program.cs

--- 06_Json/Program.cs
+++ 06_Json/Program.cs
@@ -21,6 +21,50 @@ namespace _06_Json
 
             JObject json2 = JObject.Parse(jsonstr);
             Console.WriteLine($"Name : {json2["name"]}, Salary : {json2["salary"]}");
+
+
+
+            var json1 = "[\".Net\", \"Core\", \"ASP.NET\",\"홍길동\",\"1\"]";
+            var jArray = JArray.Parse(json1);
+
+            foreach (var item in jArray.Children())
+            {
+                Console.WriteLine(item.Value<string>().ToString());
+            }
+
+            Console.WriteLine(String.Join(",", jArray.Select(i => i.ToString())));
+
+            Console.ReadKey();
+
+
+            JArray jArray2 = JArray.Parse(@"[
+                            {
+                              ""name"": ""Croke Park II"",
+            
+                                          ""url"": ""http://twitter.com/search?q=%22Croke+Park+II%22"",
+                              ""promoted_content"": null,
+                              ""query"": """" % 22Croke + Park + II % 22"",
+                              ""events"": null
+                            },
+                            {
+                              ""name"": ""Siptu"",
+                              ""url"": ""http://twitter.com/search?q=Siptu"",
+                              ""promoted_content"": null,
+                              ""query"": ""Siptu"",
+                              ""events"": null
+                            }]");
+
+            foreach (JObject item in jArray2)
+            {
+                string name = item.GetValue("name").ToString();
+                string url = item.GetValue("url").ToString();
+                // ...
+            }
+
+            var str = @"[1, 2, 3]";
+            var jArray3 = JArray.Parse(str);
+            Console.WriteLine(String.Join("-", jArray3.Where(i => (int)i > 1).Select(i => i.ToString())));
         }
+
     }
 }
